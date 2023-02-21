bb57735fcdb2aa26924a94f4bcf09052cea6cb3c
Serhii Holinei <sergey@goliney.com>
2023-02-21 16:37:49 -0500

    Favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -51,7 +51,6 @@ const App = ({ Component, pageProps }) => {
         <meta name="robots" content="index,follow" />
         <meta name="viewport" content="width=device-width" />
         <meta name="author" content="Serhii Holinei" />
-        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <Component {...pageProps} />
